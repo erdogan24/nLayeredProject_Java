8c642570995d803b6608252adbcc76e2b6ece6c0
erdoganakpolat <erdoganakpolat98@gmail.com>
2024-11-20 14:44:41 +0300

    commit - 3

---
+++ nLayeredProject_Java/src/nLayeredProject_Java/Main.java
@@ -0,0 +1,16 @@
+package nLayeredProject_Java;
+
+import nLayeredProject_Java.business.abstracts.ProductService;
+import nLayeredProject_Java.business.concretes.ProductManager;
+import nLayeredProject_Java.dataAccess.concretes.HibernateProductDao;
+import nLayeredProject_Java.entities.concretes.Product;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ProductService productService = new ProductManager(new HibernateProductDao());
+		Product product = new Product(1,2,"Erdogan",3,5);
+		productService.add(product);
+	}
+
+}
--- nLayeredProject_Java/src/nLayeredProject_Java/business/concretes/ProductManager.java
+++ nLayeredProject_Java/src/nLayeredProject_Java/business/concretes/ProductManager.java
@@ -1,5 +1,35 @@
 package nLayeredProject_Java.business.concretes;
 
-public class ProductManager {
+import java.util.List;
+
+import nLayeredProject_Java.business.abstracts.ProductService;
+import nLayeredProject_Java.dataAccess.abstracts.ProductDao;
+import nLayeredProject_Java.entities.concretes.Product;
+
+public class ProductManager implements ProductService{
+	
+	private ProductDao productDao;
+	
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public void add(Product product) {
+		
+		if(product.getCategoryId()==1) {
+			System.out.println("Bu kategoride ürün kabul edilmiyor");
+			return ;
+		}
+		
+		this.productDao.add(product);
+	}
+
+	@Override
+	public List<Product> getAll() {
+		
+		return null;
+	}
 	
 }
